9b37d32f3baa5e76e1cfef9c8c9e695749627eb3
Erica <chae_osu@yahoo.com>
2015-04-14 08:22:47 -0700

    page functions accordingly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,39 @@
-$(document).ready(function(){
-    $('form#new-contact').submit(function(event){ //check with cow?
-        event.preventDefault();
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
 
-        var inputtedFirstName = $("input#new-first-name").val();
-        var inputtedLastName = $("input#new-last-name").val();
-        var inputtedStreet = $("input#new-street").val();
-        var inputtedCity = $("input#new-city").val();
-        var inputtedState = $("input$new-state").val();
-        var newContact = {firstName: inputtedFirstName, lastName: inputtedLastName, street: inputtedStreet, city: inputtedCity, state: inputtedState };
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    //the method fullAddress() will call back the properties on the object 
+    var newAddress = { street: inputtedStreet,
+                       city: inputtedCity,
+                       state: inputtedState,
+                       fullAddress: function() {
+                         return this.street + ", " + this.city + ", " + this.state;
+                       }
+                     };
+    var newContact = { firstName: inputtedFirstName,
+                       lastName: inputtedLastName,
+                       address: newAddress.fullAddress()
+                      };
 
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 
-        $("input#new-first-name").val("");
-        $("input#new-last-name").val("");
-        $("input#new-address").val("");
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
 
-        //class contact from span class value is called back
-        $(".contact").last().click(function() {
-            $("#show-contact").show();
-            $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-            $(".first-name").text(newContact.firstName);
-            $(".last-name").text(newContact.lastName);
-            $(".address").text(newContact.address);
-        });
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".address").text(newContact.address);
     });
+  });
 });
